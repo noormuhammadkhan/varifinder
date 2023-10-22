b47b1a49cc9485259221e35f9219b59435f1bea2
Corrado Lanera <corrado.lanera@gmail.com>
2023-10-22 13:32:53 +0200

    Integrate checkmate for Argument Checks in Automated Tests
    Integrated the checkmate package to enhance argument checking in our automated tests, promoting early error detection and code robustness. Created a script that activates checkmate in the testing environment, and added a call to library(checkmate) within tests/testthat/setup.R. This setup ensures that checkmate is loaded and ready for use in every test run, aiding in maintaining a high standard of code quality and reliability as the project progresses.

--- dev/00-setup.R
+++ dev/00-setup.R
@@ -4,7 +4,12 @@ usethis::use_readme_rmd() # setup a readme with RMD superpower!
 devtools::build_readme() # update readme even if your package does not exist yet
 usethis::use_package_doc() # create file (do not touch it) for pkg documentation
 usethis::use_roxygen_md() # extremaly easily way to write func docs
+
 usethis::use_testthat() # the best (unbiased opinion!!) test environment for R!
+install.packages("checkmate")
+usethis::use_package("testthat", type = "suggests")
+usethis::use_package("checkmate", type = "suggests")
+
 usethis::use_spell_check()
 
 install.packages("lintr")
